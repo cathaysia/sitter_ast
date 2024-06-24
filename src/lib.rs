391ec2495e668954b51ea31618a2d221b74cb272
DragonBillow <DragonBillow@outlook.com>
2024-06-24 17:25:04 +0800

    feat: impl for more case
    Signed-off-by: DragonBillow <DragonBillow@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -94,34 +94,48 @@ impl RuleJSON {
                             });
                         }
                         RuleJSON::SEQ { members } => {
-                            let mut mid = quote! {};
-                            let mut childs = vec![];
-
-                            for item in members {
-                                match item {
-                                    RuleJSON::SYMBOL { name } => {
-                                        childs.push(name.clone());
-                                        let ident = ident!(&name.to_case(Case::UpperCamel));
-                                        mid.extend(quote! {
-                                            #ident,
-                                        });
-                                    }
-                                    RuleJSON::CHOICE { members: _ } => {
-                                        let ident = ident!(&format!("{ident}_TOKEN_{idx}")
-                                            .to_case(Case::UpperCamel));
-                                        res.extend(item.generate(&ident)?);
-                                        mid.extend(quote! {
-                                            #ident,
-                                        });
-                                    }
-                                    _ => {}
-                                }
-                            }
-                            let sub = ident!(&format!("TOKEN_{idx}").to_case(Case::UpperCamel));
-                            mem.extend(quote! {
-                                #sub(#mid),
+                            let is_consis_by_symbol = members.iter().all(|item| {
+                                matches!(
+                                    item,
+                                    RuleJSON::STRING { value: _ } | RuleJSON::SYMBOL { name: _ }
+                                )
                             });
+
+                            if is_consis_by_symbol {
+                                let mut mid = quote! {};
+                                let mut childs = vec![];
+
+                                for item in members {
+                                    match item {
+                                        RuleJSON::SYMBOL { name } => {
+                                            childs.push(name.clone());
+                                            let ident = ident!(&name.to_case(Case::UpperCamel));
+                                            mid.extend(quote! {
+                                                #ident,
+                                            });
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                                let sub = ident!(
+                                    &format!("{ident}_TOKEN_{idx}").to_case(Case::UpperCamel)
+                                );
+                                mem.extend(quote! {
+                                    #sub(#mid),
+                                });
+                            } else {
+                                let field_type = ident!(
+                                    &format!("{ident}_TOKEN_{idx}").to_case(Case::UpperCamel)
+                                );
+                                res.extend(item.generate(&field_type));
+                                mem.extend(quote! {
+                                    #field_type(#field_type),
+                                });
+                            }
                         }
+                        RuleJSON::BLANK => mem.extend(quote! {
+                            Blank,
+                        }),
                         _ => mid.extend(quote! {
                             "todo" => { todo!() },
                         }),
@@ -179,6 +193,43 @@ impl RuleJSON {
                                 #kind => {
                                     res.#member_name = TSParser::parse(node, source)?;
                                 }
+                            });
+                        }
+                        RuleJSON::FIELD { name: _, content } => match content.as_ref() {
+                            RuleJSON::SYMBOL { name } => {
+                                let field_name = ident!(&name.to_case(Case::Snake));
+                                let field_type = ident!(&name.to_case(Case::UpperCamel));
+                                mem.extend(quote! {
+                                    pub #field_name: #field_type,
+                                })
+                            }
+                            RuleJSON::CHOICE { members: _ } => {
+                                let name = format!("{ident}_TOKEN_{idx}");
+
+                                let field_name = ident!(&name.to_case(Case::Snake));
+                                let field_type = ident!(&name.to_case(Case::UpperCamel));
+                                res.extend(content.generate(&field_type));
+                                mem.extend(quote! {
+                                    pub #field_name: #field_type,
+                                });
+                            }
+                            _ => {}
+                        },
+                        RuleJSON::CHOICE { members: _ } => {
+                            let name = format!("{ident}_TOKEN_{idx}");
+                            let field_type = ident!(&name.to_case(Case::UpperCamel));
+                            let field_name = ident!(&name.to_case(Case::Snake));
+                            res.extend(item.generate(&field_type));
+                            mem.extend(quote! {
+                                pub #field_name: #field_type,
+                            })
+                        }
+                        RuleJSON::STRING { value } => {
+                            let name = to_ident(value);
+                            let field_name = ident!(&name.to_case(Case::Snake));
+                            let field_type = ident!(&name.to_case(Case::UpperCamel));
+                            mem.extend(quote! {
+                                pub #field_name: #field_type,
                             });
                         }
                         _ => {}
